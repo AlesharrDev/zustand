d3e5fab093d7a80f3d47f0f779efef5e2c10c680
Alesharr <27436197+LuisGermanDev@users.noreply.github.com>
2025-03-12 22:04:28 -0400

    add style form

--- app/(stack)/login/index.tsx
+++ app/(stack)/login/index.tsx
@@ -1,13 +1,34 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity } from 'react-native'
 import React from 'react'
 import globalStyle from '@/app/style/globalStyle'
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 
 const Login = () => {
-  return (
-    <View style={globalStyle.container}>
-      <Text style={globalStyle.h1}>Login</Text>
-    </View>
-  )
+    return (
+        <SafeAreaProvider>
+            <SafeAreaView style={globalStyle.container}>
+                <Text style={globalStyle.h2}>Iniciar Sesion</Text>
+                <View style={{ display: 'flex', flexDirection: 'row', marginBottom: 20 }}>
+                    <Text style={globalStyle.label}>Nombre: </Text>
+                    <TextInput
+                        placeholder="Nombre"
+                        style={globalStyle.input}
+                    />
+                </View>
+                <View style={{ display: 'flex', flexDirection: 'row', marginBottom: 20 }}>
+                    <Text style={globalStyle.label}>Email: </Text>
+                    <TextInput
+                        placeholder="Email"
+                        style={globalStyle.input}
+                        keyboardType='email-address'
+                    />
+                </View>
+                <TouchableOpacity style={globalStyle.button}>
+                    <Text style={globalStyle.buttonText}>Iniciar Sesion</Text>
+                </TouchableOpacity>
+            </SafeAreaView>
+        </SafeAreaProvider>
+    )
 }
 
 export default Login
